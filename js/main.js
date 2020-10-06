0bd6dd9d1aea4d38ceaad145fab7c44d90683e85
Dslepchenko <dslepchenko@gmail.com>
2020-10-06 22:23:51 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,63 @@
+// 'use strict';
+
+// function getData() {
+//     return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             return data;
+//         });
+// }
+
+
+
+// var arr = [];
+
+// async function init() {
+//   arr = await getData();
+// }
+
+// init();
+
+
 'use strict';
 
+//Функция для выбора случайного значения 
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+}
 
+// Функция для выбора случаного элемента массива
+function getRandomElementFromArr(arr) {
+    return arr[getRandomInRange(0, arr.length - 1)];
+}
+
+var quote = document.querySelector('.quote');
+var author = document.querySelector('.author');
+var image = document.querySelector('img');
+var button = document.querySelector('#quote-btn');
 
 function getData() {
-    fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
+    return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-
             return data;
-
         });
+}
+
+async function init() {
+    const data = await getData();
+    var randomObject = getRandomElementFromArr(data);
+    quote.innerText = randomObject.quote;
+    author.innerText = randomObject.character;
+    image.src = randomObject.image;
 
 }
 
-
-var button = document.querySelector('#quote-btn');
 
 button.addEventListener('click', function() {
-    var arrData = getData();
-    // console.log(arrData);
-
+    init();
 });
-var arr = [];
-
-function init() {
-    arr = getData();
-
-}
-init();
-console.log(arr);
