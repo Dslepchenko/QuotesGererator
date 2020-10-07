95f3d27e769a98a8a714e20939644d1ae9616418
Dslepchenko <dslepchenko@gmail.com>
2020-10-07 21:55:26 +0300

    7th commit

--- js/main.js
+++ js/main.js
@@ -4,24 +4,42 @@ const getRandomInRange = (min, max) => Math.floor(Math.random() * (max + 1 - min
 
 const getRandomElementFromArr = (arr) => arr[getRandomInRange(0, arr.length - 1)];
 
+const quoteWrapper = document.querySelector('.quote-wrapper');
 
-const quote = document.querySelector('.quote');
-const author = document.querySelector('.author');
-const image = document.querySelector('img');
-const button = document.querySelector('#quote-btn');
 const num = 5;
 
 const getData = () => fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${num}`)
     .then(response => response.json())
     .then(data => data);
 
+
+const createElement = (element, container) => {
+    const template = document.createElement(`template`);
+    template.innerHTML = element;
+    container.appendChild(template.content);
+
+};
+
+const elementTepmlate = (card) => {
+    debugger
+    return `<div class = "quote-heading">
+
+    ${(getQuotesTemplate(card.quote))}
+    ${(getImageTemplate(card.image))}
+    ${(getAuthorTemplate(card.character))}
+    </div>`
+}
+
+const getQuotesTemplate = quote => `<div>${quote}</div>`;
+const getImageTemplate = src => `<img src=${src} width="100px" height="100px">`
+const getAuthorTemplate = character => `<h2>${character}</h2>`
+
+
 const init = async() => {
     const data = await getData()
     let randomObject = getRandomElementFromArr(data);
-    quote.innerText = '"' + randomObject.quote + '"';
-    author.innerText = '-' + randomObject.character;
-    image.src = randomObject.image;
+    createElement(elementTepmlate(randomObject), quoteWrapper);
+
 }
 
-
-button.addEventListener('click', () => init());
+init();
