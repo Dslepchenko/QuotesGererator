85dc454d360edf0f659e313b8c22c232f7eeb4d8
Dslepchenko <dslepchenko@gmail.com>
2020-10-07 17:29:08 +0300

    4rd commit

--- js/main.js
+++ js/main.js
@@ -1,56 +1,28 @@
-// 'use strict';
-
-// function getData() {
-//     return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then((data) => {
-//             return data;
-//         });
-// }
-
-
-
-// var arr = [];
-
-// async function init() {
-//   arr = await getData();
-// }
-
-// init();
-
-
 'use strict';
 
-//Функция для выбора случайного значения 
 function getRandomInRange(min, max) {
     return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-// Функция для выбора случаного элемента массива
+
 function getRandomElementFromArr(arr) {
     return arr[getRandomInRange(0, arr.length - 1)];
 }
 
-var quote = document.querySelector('.quote');
-var author = document.querySelector('.author');
-var image = document.querySelector('img');
-var button = document.querySelector('#quote-btn');
+let quote = document.querySelector('.quote');
+let author = document.querySelector('.author');
+let image = document.querySelector('img');
+let button = document.querySelector('#quote-btn');
 
 function getData() {
     return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            return data;
-        });
+        .then(response => response.json())
+        .then(data => data);
 }
-
+// покажи пожалуйста, что еще тут меняется...или лучше может созвониться?
 async function init() {
-    const data = await getData();
-    var randomObject = getRandomElementFromArr(data);
+    const data = await getData()
+    let randomObject = getRandomElementFromArr(data);
     quote.innerText = '"' + randomObject.quote + '"';
     author.innerText = '-' + randomObject.character;
     image.src = randomObject.image;
