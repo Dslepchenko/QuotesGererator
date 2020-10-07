e956bc28012c2299ab5c5ffac7687f2c23f11ac3
Dslepchenko <dslepchenko@gmail.com>
2020-10-07 18:26:39 +0300

    5th commit

--- js/main.js
+++ js/main.js
@@ -1,35 +1,26 @@
 'use strict';
 
-function getRandomInRange(min, max) {
-    return Math.floor(Math.random() * (max + 1 - min) + min);
-}
+const getRandomInRange = (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
+
+const getRandomElementFromArr = (arr) => arr[getRandomInRange(0, arr.length - 1)];
 
 
-function getRandomElementFromArr(arr) {
-    return arr[getRandomInRange(0, arr.length - 1)];
-}
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+const image = document.querySelector('img');
+const button = document.querySelector('#quote-btn');
 
-let quote = document.querySelector('.quote');
-let author = document.querySelector('.author');
-let image = document.querySelector('img');
-let button = document.querySelector('#quote-btn');
+const getData = () => fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
+    .then(response => response.json())
+    .then(data => data);
 
-function getData() {
-    return fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5')
-        .then(response => response.json())
-        .then(data => data);
-}
-// покажи пожалуйста, что еще тут меняется...или лучше может созвониться?
-async function init() {
+const init = async() => {
     const data = await getData()
     let randomObject = getRandomElementFromArr(data);
     quote.innerText = '"' + randomObject.quote + '"';
     author.innerText = '-' + randomObject.character;
     image.src = randomObject.image;
-
 }
 
 
-button.addEventListener('click', function() {
-    init();
-});
+button.addEventListener('click', () => init());
