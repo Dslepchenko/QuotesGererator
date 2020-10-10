2cb77e5a618225458cd32e62b3af55958558d26f
Dslepchenko <dslepchenko@gmail.com>
2020-10-10 15:37:38 +0300

    8th commit

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const getRandomElementFromArr = (arr) => arr[getRandomInRange(0, arr.length - 1)
 
 const quoteWrapper = document.querySelector('.quote-wrapper');
 
-const num = 5;
+const num = 7;
 
 const getData = () => fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=${num}`)
     .then(response => response.json())
@@ -20,26 +20,44 @@ const createElement = (element, container) => {
 
 };
 
-const elementTepmlate = (card) => {
-    debugger
-    return `<div class = "quote-heading">
-
+const elementTepmlate = (card) => `<div class = "quote-heading hide">
+    ${(getAuthorTemplate(card.character))}
     ${(getQuotesTemplate(card.quote))}
     ${(getImageTemplate(card.image))}
-    ${(getAuthorTemplate(card.character))}
+    
     </div>`
-}
 
-const getQuotesTemplate = quote => `<div>${quote}</div>`;
-const getImageTemplate = src => `<img src=${src} width="100px" height="100px">`
+
+const getQuotesTemplate = quote => `<div class = "quote">${quote}</div>`;
+const getImageTemplate = src => `<img src=${src} class = "image">`
 const getAuthorTemplate = character => `<h2>${character}</h2>`
 
+const getValue = () => {
+    document.querySelector('#elastic').oninput = function() {
+        let val = this.value.trim(); // убираем пробелы
+        let elements = document.querySelectorAll('.quote-heading');
+        let names = elements.querySelectorAll('h2');
+        if (val !== '') {
+            names.forEach(function(name) {
+                if (name.innerText.search(val) == -1) {
+                    this.parentNode.classList.add('hide')
+                } else {
+                    this.parentNode.classList.remove('hide')
+                }
+            });
+
+        }
+    }
+
+}
 
 const init = async() => {
     const data = await getData()
-    let randomObject = getRandomElementFromArr(data);
-    createElement(elementTepmlate(randomObject), quoteWrapper);
+        // let randomObject = getRandomElementFromArr(data);
+    const result = data.map(card => elementTepmlate(card));
+    createElement(result, quoteWrapper);
 
 }
 
 init();
+getValue();
