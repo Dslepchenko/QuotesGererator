778a44a7e47aa68d4afe5d411c710b41448b1685
Dslepchenko <dslepchenko@gmail.com>
2020-10-12 00:58:22 +0300

    10th commit

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ const getData = () => fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=
     .then(response => response.json())
     .then(data => data);
 
-// создает шаблон
+
 const createElement = (element, container) => {
     const template = document.createElement(`template`);
     template.innerHTML = element;
     container.appendChild(template.content);
 
 };
-// делает рендеринг 
+
 const elementTepmlate = (card) => `<div class = "quote-heading hide">
     ${(getAuthorTemplate(card.character))}
     ${(getQuotesTemplate(card.quote))}
@@ -27,7 +27,7 @@ const elementTepmlate = (card) => `<div class = "quote-heading hide">
     
     </div>`
 
-// отрисовывает теги 
+
 const getQuotesTemplate = quote => `<div class = "quote">${quote}</div>`;
 const getImageTemplate = src => `<img src=${src} class = "image">`
 const getAuthorTemplate = character => `<h2>${character}</h2>`
@@ -36,15 +36,22 @@ const getAuthorTemplate = character => `<h2>${character}</h2>`
 var DATA = [];
 
 
-document.querySelector('#elastic').addEventListener('change', e => {
+
+document.querySelector('#elastic').addEventListener('input', e => {
     e.preventDefault();
     const value = e.target.value;
-    getFilteredCharacter(value, DATA)
+    getFilteredCharacter(value, DATA);
+    quoteWrapper.innerHTML = '';
+    const newData = getFilteredCharacter(value, DATA);
+    const renderNew = newData.map(card => elementTepmlate(card));
+    createElement(renderNew, quoteWrapper);
+
+
 })
 
 const getFilteredCharacter = (search, arr) => {
     const filterArray = arr.filter(obj => {
-        const filter = obj.character.toLowerCase().indexOf(search.toLowerCase()) !== -1 || obj.quote.toLowerCase().indexOf(search.toLowerCase()) !== -1; // я тут ищу и по character и по цитате(quote)
+        const filter = obj.character.toLowerCase().indexOf(search.toLowerCase()) !== -1 || obj.quote.toLowerCase().indexOf(search.toLowerCase()) !== -1;
 
         return filter;
     })
@@ -57,11 +64,9 @@ const getFilteredCharacter = (search, arr) => {
 
 const init = async() => {
     const data = await getData()
-        // let randomObject = getRandomElementFromArr(data);
     const result = data.map(card => elementTepmlate(card));
     createElement(result, quoteWrapper);
     DATA = data;
-    console.log(DATA);
 
 }
 
