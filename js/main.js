5285fd9b2c45f8eb4cbee02c877721a5f9af883d
Dslepchenko <dslepchenko@gmail.com>
2020-10-11 22:48:39 +0300

    9th commit

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ const getData = () => fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?count=
     .then(response => response.json())
     .then(data => data);
 
-
+// создает шаблон
 const createElement = (element, container) => {
     const template = document.createElement(`template`);
     template.innerHTML = element;
     container.appendChild(template.content);
 
 };
-
+// делает рендеринг 
 const elementTepmlate = (card) => `<div class = "quote-heading hide">
     ${(getAuthorTemplate(card.character))}
     ${(getQuotesTemplate(card.quote))}
@@ -27,37 +27,42 @@ const elementTepmlate = (card) => `<div class = "quote-heading hide">
     
     </div>`
 
-
+// отрисовывает теги 
 const getQuotesTemplate = quote => `<div class = "quote">${quote}</div>`;
 const getImageTemplate = src => `<img src=${src} class = "image">`
 const getAuthorTemplate = character => `<h2>${character}</h2>`
 
-const getValue = () => {
-    document.querySelector('#elastic').oninput = function() {
-        let val = this.value.trim(); // убираем пробелы
-        let elements = document.querySelectorAll('.quote-heading');
-        let names = elements.querySelectorAll('h2');
-        if (val !== '') {
-            names.forEach(function(name) {
-                if (name.innerText.search(val) == -1) {
-                    this.parentNode.classList.add('hide')
-                } else {
-                    this.parentNode.classList.remove('hide')
-                }
-            });
 
-        }
-    }
+var DATA = [];
+
+
+document.querySelector('#elastic').addEventListener('change', e => {
+    e.preventDefault();
+    const value = e.target.value;
+    getFilteredCharacter(value, DATA)
+})
+
+const getFilteredCharacter = (search, arr) => {
+    const filterArray = arr.filter(obj => {
+        const filter = obj.character.toLowerCase().indexOf(search.toLowerCase()) !== -1 || obj.quote.toLowerCase().indexOf(search.toLowerCase()) !== -1; // я тут ищу и по character и по цитате(quote)
+
+        return filter;
+    })
+    console.log(filterArray)
+    return filterArray;
+
 
 }
+
 
 const init = async() => {
     const data = await getData()
         // let randomObject = getRandomElementFromArr(data);
     const result = data.map(card => elementTepmlate(card));
     createElement(result, quoteWrapper);
+    DATA = data;
+    console.log(DATA);
 
 }
 
 init();
-getValue();
